ec54f35cb437d62a9649d332873dd24862def463
Heber Quequejana <heberqc@gmail.com>
2019-07-28 16:40:30 -0500

    styling components

--- src/components/containers/PlaylistItems.js
+++ src/components/containers/PlaylistItems.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import PlaylistItem from '../PlaylistItem';
+import StyledPlaylistitems from '../styles/StyledPlaylistitems';
 
 const PlaylistItems = props => (
-  <PlaylistItem />
+  <StyledPlaylistitems>
+    <PlaylistItem />
+  </StyledPlaylistitems>
 )
 
 export default PlaylistItems;
