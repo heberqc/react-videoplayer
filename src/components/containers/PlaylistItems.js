7a591060e65c7ee4fb6785d2509a6ca365fc40f5
Heber Quequejana <heberqc@gmail.com>
2019-07-29 12:00:18 -0500

    add a high order component

--- src/components/containers/PlaylistItems.js
+++ src/components/containers/PlaylistItems.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PlaylistItem from '../PlaylistItem';
 import StyledPlaylistitems from '../styles/StyledPlaylistitems';
+import withLink from '../hoc/withLink';
+
+const PlaylistItemWithLink = withLink(PlaylistItem);
 
 const PlaylistItems = ({ videos, active, ...props }) => (
   <StyledPlaylistitems>
     {videos.map(video => (
-      <PlaylistItem
+      <PlaylistItemWithLink
         key={video.id}
         video={video}
         active={video.id === active.id}
